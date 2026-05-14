996748379516c2437fba7517b8197d7217e518fe
Kevin Messier <kevin.messier@etu.cegepdrummond.ca>
2026-05-14 15:33:55 -0400

    add migration

---
+++ backend/BudgetManager/BudgetManager.Domain/Entities/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetManager.Domain.Entities
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = null!;
+        public Guid CategoryId { get; set; }
+    }
+}
---
+++ backend/BudgetManager/BudgetManager.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BudgetManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetManager.Infrastructure.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne<Category>()
+                .WithMany()
+                .HasForeignKey(b => b.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict); // BLOQUE la suppression de la catégorie
+
+            modelBuilder.Entity<Transaction>()
+                .Property(c => c.Amount)
+                .HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
