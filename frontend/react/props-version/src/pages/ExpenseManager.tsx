cbb30d7cb7117f906d1c1a9ca3f1cd83eb6da822
Kevin Messier <kmessier98@hotmail.com>
2026-05-19 21:26:00 -0400

    start frontend

---
+++ frontend/react/props-version/src/pages/ExpenseManager.tsx
@@ -0,0 +1,14 @@
+import "./ExpenseManager.scss";
+
+const ExpenseManager = () => {
+  return (
+    <div className="expense-manager">
+      <div className="expense-manager-content">
+        <img></img>
+        <h1>Gestionnaire de dépenses</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseManager;
