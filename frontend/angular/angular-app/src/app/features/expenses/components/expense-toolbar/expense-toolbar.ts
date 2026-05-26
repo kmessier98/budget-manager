a5056fde19d1d40d8d4c95deee5963b88b12dd4e
Kevin Messier <kevin.messier@etu.cegepdrummond.ca>
2026-05-26 16:55:06 -0400

    avancement..

--- frontend/angular/angular-app/src/app/features/expenses/components/expense-toolbar/expense-toolbar.ts
+++ frontend/angular/angular-app/src/app/features/expenses/components/expense-toolbar/expense-toolbar.ts
@@ -1,9 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, Signal, signal } from '@angular/core';
+import { SelectModule } from 'primeng/select';
+import { Filters } from '../../models/expense/expense';
+import { Button } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-expense-toolbar',
-  imports: [],
+  imports: [SelectModule, Button, FormsModule],
   templateUrl: './expense-toolbar.html',
   styleUrl: './expense-toolbar.scss',
 })
-export class ExpenseToolbar {}
+export class ExpenseToolbar {
+  @Input() filters: Filters = {
+    year: '',
+    month: '',
+    day: '',
+    categoryId: '',
+  };
+  @Input() daysInMonth: Signal<{ value: string; label: string }[]> = signal([]);
+  @Input() categories: { value: string; label: string }[] = [];
+  @Output() onFiltersChange = new EventEmitter<Filters>();
+  @Output() onAddExpenseSuccess = new EventEmitter<void>();
+
+  private readonly START_YEAR = 1900;
+  private readonly CURRENT_YEAR = new Date().getFullYear();
+  readonly months = [
+    { value: '', label: 'Aucun' },
+    { value: '1', label: 'Janvier' },
+    { value: '2', label: 'Février' },
+    { value: '3', label: 'Mars' },
+    { value: '4', label: 'Avril' },
+    { value: '5', label: 'Mai' },
+    { value: '6', label: 'Juin' },
+    { value: '7', label: 'Juillet' },
+    { value: '8', label: 'Août' },
+    { value: '9', label: 'Septembre' },
+    { value: '10', label: 'Octobre' },
+    { value: '11', label: 'Novembre' },
+    { value: '12', label: 'Décembre' },
+  ];
+  years = [{ value: this.CURRENT_YEAR.toString(), label: this.CURRENT_YEAR.toString() }];
+
+  ngOnInit() {
+    this.initYears();
+    console.log('filters in toolbar:', this.filters);
+  }
+
+  initYears() {
+    for (let year = this.CURRENT_YEAR - 1; year >= this.START_YEAR; year--) {
+      this.years.push({
+        value: year.toString(),
+        label: year.toString(),
+      });
+    }
+  }
+
+  handleYearChange = () => {
+    this.filters.day = '';
+    this.onFiltersChange.emit({
+      ...this.filters,
+    });
+  };
+
+  handleMonthChange = () => {
+    this.filters.day = '';
+    this.onFiltersChange.emit({
+      ...this.filters,
+    });
+  };
+
+  handleDayChange = () => {
+    this.onFiltersChange.emit({
+      ...this.filters,
+    });
+  };
+
+  handleCategoryChange = () => {
+    this.onFiltersChange.emit({
+      ...this.filters,
+    });
+  };
+}
--- frontend/angular/angular-app/src/app/features/expenses/pages/expense-manager/expense-manager.ts
+++ frontend/angular/angular-app/src/app/features/expenses/pages/expense-manager/expense-manager.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { ExpenseToolbar } from '../../components/expense-toolbar/expense-toolbar';
 import { ExpenseTotalAmount } from '../../components/expense-total-amount/expense-total-amount';
 import { ExpenseTable } from '../../components/expense-table/expense-table';
 import { ExpenseChart } from '../../components/expense-chart/expense-chart';
 import { ButtonModule } from 'primeng/button';
+import { Filters } from '../../models/expense/expense';
 
 @Component({
   selector: 'app-expense-manager',
@@ -13,4 +14,33 @@ import { ButtonModule } from 'primeng/button';
 })
 export class ExpenseManager {
   avatar = '/assets/user-avatar.png';
+  filters: Filters = {
+    year: new Date().getFullYear().toString(),
+    month: new Date().getMonth().toString(),
+    day: new Date().getDay().toString(),
+    categoryId: '',
+  };
+
+  onFiltersChange = (filters: Filters) => {
+    this.filters = filters;
+    console.log('parent!!!:', this.filters);
+  };
+
+  //todo a déplacé dans l'enfant... mais aavnt comprendre comment ma rappelé l'enfant apres un changement de filtre (peut etre que ca ne va pas retoruenr dans lenfant au final)
+  //filter devra etre un signal pour qu ewceci marche...
+  daysInMonth = computed(() => {
+    const month = parseInt(this.filters.month);
+    const year = parseInt(this.filters.year);
+
+    if (!month) return [{ value: '', label: 'Aucun' }];
+
+    const numberOfDays = new Date(year, month, 0).getDate();
+    const newDays = [{ value: '', label: 'Aucun' }];
+
+    for (let day = 1; day <= numberOfDays; day++) {
+      newDays.push({ value: day.toString(), label: day.toString() });
+    }
+    console.log('days in month:', newDays);
+    return newDays;
+  });
 }
