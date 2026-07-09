4d88a69e0adc6752feeb39a4d657c1e5c59b80c8
Kevin <kmessier98@hotmail.com>
2026-07-09 15:25:21 -0400

    logout

--- backend/BudgetManager/BudgetManager.API/Controllers/AuthController.cs
+++ backend/BudgetManager/BudgetManager.API/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using BudgetManager.Application.DTOs.Auth;
 using BudgetManager.Application.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -42,6 +44,21 @@ namespace BudgetManager.API.Controllers
             Response.Cookies.Append("jwt", result.Token ?? string.Empty, cookieOptions);
 
             return result.IsSuccess ? Ok(new { result.Message }) : Unauthorized(result);
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = !_env.IsDevelopment(),
+                SameSite = SameSiteMode.Strict,
+            };
+
+            Response.Cookies.Delete("jwt", cookieOptions);
+
+            return Ok(new { message = "Déconnexion réussie" });
         }
     }
 }
